ea83b8affe6211c20f188386f70ecbfa14f3b4c8
Ashish Namdev <ashish28.sirt@gmail.com>
2016-01-23 19:29:47 +0530

    Implemnted User snap for idroc.User database
    User.java model presents idroc.USer table from mysql idroc
    databse in idroc project
    
    Signed-off-by: Ashish Namdev <ashish28.sirt@gmail.com>

--- src/model/User.java
+++ src/model/User.java
@@ -9,6 +9,73 @@ package model;
  *
  * @author anamdev
  */
-public class User {
+public class User
+{
+    private String uid;
+    private String email;
+    private String password;
+    private String userName;
+
+    public User()
+    {
+        this.uid = null;
+        this.email = null;
+        this.password = null;
+        this.userName = null;
+    }
+
+    public User(String uid, String email, String password, String userName)
+    {
+        this.uid = uid;
+        this.email = email;
+        this.password = password;
+        this.userName = userName;
+    }
+
+    public String getUid()
+    {
+        return uid;}
+
+    public void setUid(String uid)
+    {
+        this.uid = uid;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public void addUSer()
+    {
+    }
     
+    public void deleteUser()
+    {
+    }
 }
