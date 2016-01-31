f18e3ec9af3d8d70f771399a5a12c736f3e22f86
Ashish Namdev <ashish28.sirt@gmail.com>
2016-02-01 02:23:28 +0530

    enabling search feature for inventories
    user or admin is able to search inventory with filter patterns
    like name location and type of inventory
    
    Signed-off-by: Ashish Namdev <ashish28.sirt@gmail.com>

--- src/comms/Query.java
+++ src/comms/Query.java
@@ -15,7 +15,11 @@ public class Query
     public static final String USERSELECT = "SELECT uid , f_name ,m_name , l_name from idroc.user where email = ? and password = ? ";
     public static final String USERCHECK  = "SELECT * FROM idroc.user where email = ? ";
     
-    public static final String INVENTORYINSERT = "insert into inventory(inv_id, name , detail, type, location) values(?,?,?,?,?)";
-    public static final String INVENTORYCHECK = "Select * from idroc.inventory where inv_id = ? ";
+    public static final String INVENTORYINSERT = "INSERT INTO inventory(inv_id, name , detail, type, location) values(?,?,?,?,?)";
+    public static final String INVENTORYCHECK = "SELECT * FROM idroc.inventory where inv_id = ? ";
     public static final String INVENTORYDELETE = "DELETE FROM  idroc.inventory where inv_id = ? ";
+
+    public static final String INVENTORYSEARCHBYLOCATION = "SELECT * FROM  idroc.inventory where location LIKE ? ";
+    public static final String INVENTORYSEARCHBYNAME = "SELECT * FROM  idroc.inventory where name LIKE ? ";
+    public static final String INVENTORYSEARCHBYTYPE = "SELECT * FROM  idroc.inventory where type LIKE ? ";
 }
